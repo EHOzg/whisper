23a5d90c57bfbb961558ac265a3ea5f4ac837281
zg <zg@secxun.com>
2026-04-09 17:37:29 +0800

    chore: disable automatic redirection to default locale in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
     locales: ['zh', 'en'],
     routing: {
       prefixDefaultLocale: false,
-      redirectToDefaultLocale: true
+      redirectToDefaultLocale: false
     }
   },
   vite: {
